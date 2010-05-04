fdf9e8621aac8342df6301211c7b3037b53967ef
Andrew Brookins <andrew@redspire.net>
2010-05-04 12:45:07 -0700

    typo

--- booker_example.rb
+++ booker_example.rb
@@ -5,7 +5,7 @@ b = Booker.new('http://www.multcolib.org/', {
 	:card => 'your library card number',
 	:pin => 'your libary card pin',
 	:email => 'your gmail address',
-	:pass => 'you gmail password',
+	:pass => 'your gmail password',
 	:to => 'the email address to send notifications to'
 })
 
