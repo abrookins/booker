f1f672ca1bc311bb1b9b5a381242ebaa3b6aac6b
Andrew Brookins <andrew@redspire.net>
2010-05-04 09:56:03 -0700

    make a better example

--- booker_example.rb
+++ booker_example.rb
@@ -2,11 +2,11 @@
 require File.dirname(__FILE__) + '/lib/booker.rb'
 
 b = Booker.new('http://www.multcolib.org/', {
-	:card = 'your library card number',
-	:pin = 'your libary card pin',
-	:email = 'your gmail address',
-	:pass = 'you gmail password',
-	:to = 'the email address to send notifications to'
+	:card => 'your library card number',
+	:pin => 'your libary card pin',
+	:email => 'your gmail address',
+	:pass => 'you gmail password',
+	:to => 'the email address to send notifications to'
 })
 
 b.renew_books
