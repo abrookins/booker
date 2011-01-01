db4c4c8346d8bc45e08136e3d97a5d65369c50f4
Andrew Brookins <a.m.brookins@gmail.com>
2011-01-01 11:33:48 -0800

    report more book status messages

--- lib/booker.rb
+++ lib/booker.rb
@@ -3,68 +3,89 @@
 require 'rubygems'
 require 'mechanize' 
 require 'tlsmail'
+require 'ostruct'
 
 class Booker 
-	def initialize(uri, config) 
-		@agent = Mechanize.new 
-		@home_page = @agent.get(uri)
+    def initialize(url, config) 
+        @agent = Mechanize.new 
+        @library_url = url
+        @home_page = nil
+        @config = config 
+        @book_groups = [
+            new_book_group(/ON HOLD/, 'on hold'),
+            new_book_group(/RENEWED/, 'renewed'),
+            new_book_group(/TOO SOON TO RENEW/, 'not renewed (too soon)')
+        ]
+        self.validate(@config)
+    end
+    def new_book_group(pattern, message)
+        OpenStruct.new({
+            'pattern' => pattern,
+            'message' => message,
+            'found' => []
+        })
+    end
+    def validate(config)
+        [:card, :pin, :email, :pass, :to].each do |key|
+            unless config.has_key?(key)
+                raise "Config missing #{key.to_str} key."
+            end
+        end
+    end
+    def login
+        @account_page = @agent.click(@home_page.link_with(:text => 'My account'))
+        login_form = @account_page.form('patform')
+        login_form.code = @config[:card]
+        login_form.pin = @config[:pin]
+        @user_page = @agent.submit(login_form, login_form.buttons.first)
+    end
+    def renew_books
+        target = '' 
+        @home_page = @agent.get(@library_url)
 
-		validate(config)
-		@config = config 
-	end
-	def validate(config)
-		[:card, :pin, :email, :pass, :to].each do |key|
-			unless config.has_key?(key)
-				raise "Config missing #{key.to_str} key."
-			end
-		end
-	end
-	def login
-		@account_page = @agent.click(@home_page.link_with(:text => 'My account'))
-		login_form = @account_page.form('patform')
-		login_form.code = @config[:card]
-		login_form.pin = @config[:pin]
-		@user_page = @agent.submit(login_form, login_form.buttons.first)
-	end
-	def renew_books
-		target = '' 
-		renew_uri = ''
-		holds = ''
-                
-		login 
+        def row_content_with_pattern(pattern, row)
+            row.content.gsub(/\n/, "") + "\n" if pattern.match(row.content)
+        end 
 
-		@user_page.links.each do |link|
-			target = link.text.strip if /Items currently checked out/.match(link.text.strip)
-		end
+        self.login 
 
-		@renew_page = @agent.click(@user_page.link_with(:text => target))
+        @user_page.links.each do |link|
+            target = link.text.strip if /Items currently checked out/.match(link.text.strip)
+        end
 
-		# The link for the 'renew all' button is JavaScript-enabled to send 
-		# the user to the current URI with an added GET param.
-		@result_page = @agent.get(@renew_page.uri + '?action=renewall')
+        @renew_page = @agent.click(@user_page.link_with(:text => target))
 
-		# Search for any renewal messages
-		@result_page.search('tr.patFuncEntry').each do |row|
-			holds = holds + row.content.gsub(/\n/, '') + "\n" if /ON HOLD/.match(row.content)
-		end
-		
-		# Send an email if it's time to give up the books 
-		notify('on hold', holds) unless holds.empty?
-	end
-	def notify(message, books)
-		uc_message = message.upcase
-		content = <<ENDOFMESSAGE
-From: Me <#{@config[:email]}>
-To: Me <#{@config[:to]}>
-Subject: Library Books #{uc_message} 
+        # The link for the 'renew all' button is JavaScript-enabled to send 
+        # the user to the current URI with an added GET param.
+        @result_page = @agent.get(@renew_page.uri + '?action=renewall')
 
-The following books are #{message}:
-#{books}
-ENDOFMESSAGE
+        # Search for any renewal messages
+        @result_page.search('tr.patFuncEntry').each do |row|
+            @book_groups.each do |group|
+                content = row_content_with_pattern(group.pattern, row)
+                group.found << content if content  
+            end
+        end
 
-		Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-		Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', @config[:email], @config[:pass], :login) do |smtp|
-			smtp.send_message(content, @config[:email], @config[:to]) 
-		end
-	end
+        notify(@book_groups)
+    end
+    def notify(book_groups)
+        notices = ""
+        content = "From: Me <#{@config[:email]}>\n"
+        content += "To: Me <#{@config[:to]}>\n"
+        content += "Subject: Library Books\n\n"
+
+        book_groups.each do |group|
+            group_string = "The following books were %s:\n %s"
+            notices += group_string % [group.message, group.found] if not group.found.empty?
+        end
+
+        notices = "Nothing to update." if notices.empty?
+        content += notices
+
+        Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+        Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', @config[:email], @config[:pass], :login) do |smtp|
+            smtp.send_message(content, @config[:email], @config[:to]) 
+        end
+    end
 end
